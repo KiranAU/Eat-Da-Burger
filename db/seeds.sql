2317bfedce89c6b51f384d7e6da15c3b3df12e11
KiranAU <kiranuttarkar@Emilys-MacBook-Air.local>
2017-08-12 07:47:27 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon King', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Son-of-Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Siracha Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
